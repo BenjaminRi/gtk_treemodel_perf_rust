575654f0d008aaec6ac2ba3abb3bb5da5a0ca49f
Benjamin <noreply@example.com>
2020-05-03 18:16:40 +0200

    Clean up, make code exactly like C repro

--- src/main.rs
+++ src/main.rs
@@ -12,94 +12,73 @@ use gtk::{
 };
 
 enum Columns {
-	FirstU32 = 0,
+	U64Column = 0,
 }
 
-fn eval_perf(elems: usize) {
-	let my_store = TreeStore::new(&[glib::Type::U32]);
-	//let my_store_sort = TreeModelSort::new(&my_store);
-
-	let column = TreeViewColumn::new();
-	column.set_title("Column header title");
-
-	let renderer_text = CellRendererText::new(); //Renderer not relevant to this performance problem
-	column.pack_start(&renderer_text, false);
-	column.add_attribute(&renderer_text, "text", Columns::FirstU32 as i32);
+fn create_store(size: usize) -> TreeStore {
+	let treestore = TreeStore::new(&[glib::Type::U32]);
+	//Note: If we add a sorted model, the runtime explodes even more
+	//let my_store_sort = TreeModelSort::new(&treestore);
 	
-	let numbers : Vec<u32> = (0..elems as u32).collect();
-	for number in numbers {
-		my_store.insert_with_values(None, None, &[Columns::FirstU32 as u32], &[&number]);
-	}
-	
-	let iter = my_store.get_iter(&gtk::TreePath::new_first()).unwrap();
-	
-	let new_val = (99 as u32).to_value();
-	let now = SystemTime::now();
-	
-	//In the following loop, we set 5000 u32-elements
-	//This operation takes roughly 240ms on my machine
-	loop {
-		/*let _n_u32 = my_store
-			.get_value(&iter, Columns::FirstU32 as i32)
-			.get_some::<u32>()
-			.unwrap();
-		
-		//println!("{}", _n_u32);*/
-		
-		my_store.set_value(
-			&iter,
-			Columns::FirstU32 as u32,
-			&new_val,
-		);
-	
-		if !my_store.iter_next(&iter) {
-			break;
+	{
+		let numbers : Vec<u64> = (0..size as u64).rev().collect();
+		for number in numbers {
+			let iter = treestore.append(None);
+			//treestore.set_value(&iter, Columns::U64Column as u32, &number.to_value());
 		}
 	}
-	match now.elapsed() {
-		Ok(elapsed) => {
-			println!(
-				"{},{}",
-				elems, elapsed.as_secs() * 1000 + elapsed.subsec_millis() as u64
-			);
-		}
-		Err(e) => {
-			// an error occurred!
-			println!("Error: {:?}", e);
-		}
-	}
-}
-
-fn build_ui(application: &gtk::Application) {
-	let window = ApplicationWindow::new(application);
-
-	window.set_title("TreeView performance test");
-	window.set_position(WindowPosition::Center);
-
-	let step = 5000;
-	let max = 5000;
-	for i in (step..=max).step_by(step) {
-		eval_perf(i);
-	}
-
-	/*
-	//View/Window not relevant to this performance problem
-	let attach_to_view = true;
-	if attach_to_view {	
-		let my_tree_view = TreeView::new_with_model(&my_store_sort);
-		my_tree_view.set_headers_visible(true);
-		my_tree_view.append_column(&column);
-		let attach_to_window = true;
-		if attach_to_window {
-			let scrolled_window = gtk::ScrolledWindow::new(gtk::NONE_ADJUSTMENT, gtk::NONE_ADJUSTMENT);
-			scrolled_window
-				.set_property("overlay-scrolling", &false)
+	
+	{
+		let iter = treestore.get_iter_first().unwrap();
+		let new_val = (99 as u64).to_value();
+		let time_before = SystemTime::now();
+		loop {
+			//In this loop, we set `size` u32-elements
+			//This operation is somehow taking quadratic amounts of time
+			
+			/*let _n_u64 = treestore
+				.get_value(&iter, Columns::U64Column as i32)
+				.get_some::<u64>()
 				.unwrap();
-			scrolled_window.set_policy(gtk::PolicyType::Never, gtk::PolicyType::Automatic);
-			scrolled_window.add(&my_tree_view);
-			window.add(&scrolled_window);
+			
+			//println!("{}", _n_u64);*/
+			
+			treestore.set_value(&iter,Columns::U64Column as u32, &new_val);
+		
+			if !treestore.iter_next(&iter) {
+				break;
+			}
 		}
-	}*/
+		let elapsed = time_before.elapsed().unwrap();
+		println!("{},{}", size, elapsed.as_secs() * 1000 + elapsed.subsec_millis() as u64);
+	}
+	return treestore;
+}
+
+fn activate(application: &gtk::Application) {
+	let treestore = create_store(15000);
+	
+	let view = TreeView::new_with_model(&treestore);
+	
+	let column = TreeViewColumn::new();
+	column.set_title("TreeModel Perf");
+
+	let renderer_text = CellRendererText::new();
+	column.pack_start(&renderer_text, false);
+	column.add_attribute(&renderer_text, "text", Columns::U64Column as i32);
+	view.append_column(&column);
+	
+	let scrolled_window = gtk::ScrolledWindow::new(gtk::NONE_ADJUSTMENT, gtk::NONE_ADJUSTMENT);
+	scrolled_window.set_policy(
+		gtk::PolicyType::Automatic,
+		gtk::PolicyType::Automatic);
+	scrolled_window.set_property("overlay-scrolling", &false).unwrap();
+	scrolled_window.add(&view);
+
+	let window = ApplicationWindow::new(application);
+	window.set_title("TreeModel Perf");
+	window.set_default_size(400, 400);
+	window.add(&scrolled_window);
 	window.show_all();
 }
 
@@ -111,7 +90,7 @@ fn main() {
 	.expect("Initialization failed...");
 
 	application.connect_activate(|app| {
-		build_ui(app);
+		activate(app);
 	});
 
 	application.run(&args().collect::<Vec<_>>());
